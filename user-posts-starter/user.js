37dc5be72498327bf4d0c96665e8d42cc49a58ab
Wendy <giswsh@yahoo.com>
2025-06-12 14:28:33 -0600

    updates for FinalStarterJavaScript

--- user-posts-starter/user.js
+++ user-posts-starter/user.js
@@ -1,9 +1,9 @@
-console.log();
-
 async function main() {
     const id = localStorage.getItem("id");
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = posts.json();
+    const postsData = await posts.json();
 
     console.log (postData);
     }
+
+main();
