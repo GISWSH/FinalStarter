a1519b270c29db04a41950a678fd928d5c482413
Wendy <giswsh@yahoo.com>
2025-06-18 15:15:36 -0600

    change

--- user-posts-starter/user.js
+++ user-posts-starter/user.js
@@ -1,10 +1,19 @@
 const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
     const id = event.target.value;
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+    renderPosts(id)
+}
+
+async function renderPosts(id){
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
-    postListEl.innerHTML = postsData.map(post => `
+    postListEl.innerHTML = postsData.map(post => postHTML (post)).join('');
+}
+
+function postHTML (post) {
+    return `
         <div class="post">
           <div class="post__title">
             ${post.title}
@@ -13,24 +22,7 @@ async function onSearchChange(event) {
         ${post.body}
         </p>
         </div>
-    `).join('');
+    `
 }
 
-async function renderPosts() {
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-    console.log (postsData);
-
-    postListEl.innerHTML = postsData.map(post => `
-        <div class="post">
-        <div class="post__title">
-        ${post.title}
-        </div>
-        <p class="post__body">
-        ${post.body}
-        </p>
-        </div>
-    `).join('');
-    }
-
-main();
+renderPosts(id);
